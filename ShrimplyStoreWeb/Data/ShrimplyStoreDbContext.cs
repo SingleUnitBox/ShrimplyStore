282efae12e5921e1686a5e315bb90babf74743a7
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-04-05 23:07:40 +0100

    add Category

---
+++ ShrimplyStoreWeb/Data/ShrimplyStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShrimplyStoreWeb.Models;
+
+namespace ShrimplyStoreWeb.Data
+{
+    public class ShrimplyStoreDbContext : DbContext
+    {
+        public ShrimplyStoreDbContext(DbContextOptions<ShrimplyStoreDbContext> options) :base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
