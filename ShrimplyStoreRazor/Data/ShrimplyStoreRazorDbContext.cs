bfc64d2f3727aad80f094475680b539379834e39
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-04-13 22:33:07 +0100

    add RazorPages

---
+++ ShrimplyStoreRazor/Data/ShrimplyStoreRazorDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShrimplyStoreRazor.Models;
+
+namespace ShrimplyStoreRazor.Data
+{
+    public class ShrimplyStoreRazorDbContext : DbContext
+    {
+        public ShrimplyStoreRazorDbContext(DbContextOptions<ShrimplyStoreRazorDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Species> Species { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Species>().HasData(
+                new Species { Id = 1, Name = "Caridina", DisplayOrder = 1},
+                new Species { Id = 2, Name = "Neo", DisplayOrder = 2},
+                new Species { Id = 3, Name = "Sula", DisplayOrder = 3}
+                );
+        }
+    }
+}
