59ca21cf0bd9f6123999915403aea22f352082e2
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-04-18 23:44:17 +0100

    add Company DataTables

---
+++ ShrimplyStoreWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shrimply.DataAccess.Repository.IRepository;
+using Shrimply.Models;
+
+namespace ShrimplyStoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Company> companies = _unitOfWork.Companies.GetAll().ToList();
+            return View(companies);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companies = _unitOfWork.Companies.GetAll().ToList();
+            return Json(new { data = companies });
+        }
+        #endregion
+    }
+}
