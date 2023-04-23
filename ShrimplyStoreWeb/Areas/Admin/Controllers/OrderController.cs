de9772e25bd7a3c84d54899a20fdb258cf822add
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-04-23 22:41:21 +0100

    add OrderController

---
+++ ShrimplyStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Shrimply.DataAccess.Repository.IRepository;
+using Shrimply.Models;
+using Shrimply.Models.ViewModels;
+using Shrimply.Utility;
+using System.Diagnostics;
+
+namespace ShrimplyStoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int orderId) 
+        {
+            OrderViewModel orderViewModel = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeaders.Get(x => x.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(x => x.OrderHeaderId == orderId, includeProperties: "Shrimp"),
+            };
+            return View(orderViewModel);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaderList = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaderList = orderHeaderList.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaderList });
+        }
+
+        #endregion
+    }
+}
